bf2467589966223128c87f52924a53a186ddd61f
hohshho <tkdgur8377@gmail.com>
2025-04-09 22:30:24 +0900

    url vercel로 변경

--- section02/src/lib/fetch-books.ts
+++ section02/src/lib/fetch-books.ts
@@ -1,7 +1,7 @@
 import { BookData } from "@/types";
 
 export default async function fetchBooks(q?:string) : Promise<BookData[]>{
-    let url = `http://localhost:12345/book`;
+    let url = `onebite-books-server-main-gamma-teal.vercel.app/book`;
 
     if(q) {
         url += `/search?q=${q}`
