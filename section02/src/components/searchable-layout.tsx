ec1d345ff802cfd6bdd66728d33958f150b341cb
hshoon <hshoon@egiskorea.com>
2025-04-09 10:31:18 +0900

    Component화, 전달받은 type 정의의

---
+++ section02/src/components/searchable-layout.tsx
@@ -0,0 +1,11 @@
+import { ReactNode } from "react";
+
+export default function SearchableLayout({children} : {children:ReactNode}) {
+
+    return (
+        <div>
+            <div>임시 서치 바</div>
+            {children}
+        </div>
+    )
+}
