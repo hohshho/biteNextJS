a1399cd9215bb5921fce156b008548d3db5acf70
hshoon <hshoon@egiskorea.com>
2025-04-10 09:57:30 +0900

    layout

--- section03/src/app/(with-searchbar)/page.tsx
+++ section03/src/app/(with-searchbar)/page.tsx
@@ -1,7 +1,9 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import { useEffect } from "react";
 
 export default function Home() {
+
   return (
     <div className={styles.page}>
         인덱스 페이지
