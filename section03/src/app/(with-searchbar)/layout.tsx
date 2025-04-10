77c391a87ad1469ec94bfb2574ff8dab9dcc5a35
hshoon <hshoon@egiskorea.com>
2025-04-10 11:30:39 +0900

    3.7 UI Update

--- section03/src/app/(with-searchbar)/layout.tsx
+++ section03/src/app/(with-searchbar)/layout.tsx
@@ -1,12 +1,11 @@
-import { ReactNode } from "react";
-import Searchbar from "./searchbar";
+import { ReactNode, Suspense } from "react";
+import Searchbar from "../../components/searchbar";
 
-export default function Layout(
-    {children} :
-    {children: ReactNode}
-) {
-  return <div>
-    <Searchbar />
-    {children}
-  </div>;
+export default function Layout({ children }: { children: ReactNode }) {
+  return (
+    <div>
+      <Searchbar />
+      {children}
+    </div>
+  );
 }
