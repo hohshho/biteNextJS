0fa693b824e7591aedf7ddaebe0a4d94a2a571da
hohshho <tkdgur8377@gmail.com>
2025-04-15 23:02:34 +0900

    delete기능 추가

--- section08/src/components/review-item.tsx
+++ section08/src/components/review-item.tsx
@@ -1,5 +1,6 @@
 import { ReviewData } from "@/types";
 import style from "./review-item.module.css";
+import ReviewItemDeleteButton from "./review-item-delete-button";
 
 export default function ReviewItem({
   id,
@@ -13,8 +14,10 @@ export default function ReviewItem({
       <div className={style.author}>{author}</div>
       <div className={style.content}>{content}</div>
       <div className={style.bottom_container}>
-        <div className={style.date}>{new Date(createdAt).toLocaleDateString()}</div>
-        <div className={style.delete_btn}>삭제하기</div>
+        <div className={style.date}>
+          {new Date(createdAt).toLocaleDateString()}
+        </div>
+        <ReviewItemDeleteButton reviewId={id} bookId={bookId} />
       </div>
     </div>
   );
