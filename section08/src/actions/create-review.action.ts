25d828d102ea3571c1b2a3cb4cbf8e5652865e34
hohshho <tkdgur8377@gmail.com>
2025-04-15 22:17:04 +0900

    useActionState

--- section08/src/actions/create-review.action.ts
+++ section08/src/actions/create-review.action.ts
@@ -3,13 +3,16 @@
 import { revalidatePath, revalidateTag } from "next/cache";
 import { delay } from "@/util/delay";
 
-export async function createReviewAction(formData: FormData) {
+export async function createReviewAction(_: any, formData: FormData) {
   const bookId = formData.get("bookId")?.toString();
   const content = formData.get("content")?.toString();
   const author = formData.get("author")?.toString();
 
   if (!bookId || !content || !author) {
-    return;
+    return {
+      status: false,
+      error: "리뷰 내용과 작성자를 입력해주세요.",
+    };
   }
 
   try {
@@ -21,6 +24,9 @@ export async function createReviewAction(formData: FormData) {
         body: JSON.stringify({ bookId, content, author }),
       }
     );
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
     console.log(response.status);
 
     // // 1. 특정 주소의 해당하는 페이지만 검증
@@ -37,8 +43,15 @@ export async function createReviewAction(formData: FormData) {
 
     // 5. 태그를 기준, 데이터 캐시 재검증
     revalidateTag(`review-${bookId}`);
+    return {
+      status: true,
+      error: "",
+    };
   } catch (err) {
     console.error(err);
-    return;
+    return {
+      status: false,
+      error: "리뷰 작성을 실패했습니다.",
+    };
   }
 }
