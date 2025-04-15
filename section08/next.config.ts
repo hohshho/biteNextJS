5d80de7000cf63922696c4b7f5d5613d3e59f347
hshoon <hshoon@egiskorea.com>
2025-04-15 15:54:30 +0900

    section8 init

---
+++ section08/next.config.ts
@@ -0,0 +1,16 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  experimental: {
+    typedRoutes: false,  // ← 이걸 통해 타입 충돌 완화 가능
+    // fetch instrumentation은 아직 공식 도큐먼트엔 없음
+  },
+  logging: {
+    fetches: {
+      fullUrl: true,
+    }
+  }
+};
+
+export default nextConfig;
