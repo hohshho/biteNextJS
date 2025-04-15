33b45efa3b6a55c9fd9d7034cf1fc6bcb601edc8
hohshho <tkdgur8377@gmail.com>
2025-04-15 21:32:23 +0900

    페이지 재검증 추가

--- section08/src/actions/create-review.action.ts
+++ section08/src/actions/create-review.action.ts
@@ -1,5 +1,8 @@
 "use server";
 
+import { revalidatePath, revalidateTag } from "next/cache";
+import { delay } from "@/util/delay";
+
 export async function createReviewAction(formData: FormData) {
   const bookId = formData.get("bookId")?.toString();
   const content = formData.get("content")?.toString();
@@ -10,6 +13,7 @@ export async function createReviewAction(formData: FormData) {
   }
 
   try {
+    await delay(2000);
     const response = await fetch(
       `${process.env.NEXT_PUBLIC_API_SERVER_URL}/review`,
       {
@@ -18,6 +22,21 @@ export async function createReviewAction(formData: FormData) {
       }
     );
     console.log(response.status);
+
+    // // 1. 특정 주소의 해당하는 페이지만 검증
+    // revalidatePath(`/book/${bookId}`);
+
+    // // 2. 특정 경로의 모든 동적 페이지를 재검증
+    // revalidatePath("/book/[id]", "page");
+
+    // // 3. 특정 레이아웃을 갖는 모든 페이지 재검증
+    // revalidatePath("/(with-searchbbar)", "layout");
+
+    // // 4. 모든 데이터 재검증
+    // revalidatePath("/", "layout");
+
+    // 5. 태그를 기준, 데이터 캐시 재검증
+    revalidateTag(`review-${bookId}`);
   } catch (err) {
     console.error(err);
     return;
