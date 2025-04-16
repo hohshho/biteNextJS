19ed3f7f0a2702a31df1361a36f10f08bc58e158
hshoon <hshoon@egiskorea.com>
2025-04-16 16:52:42 +0900

    10 init, image 최적화화

---
+++ section10/src/app/(with-searchbar)/search/error.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { startTransition, useEffect } from "react";
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error;
+  reset: () => void;
+}) {
+    const router = useRouter();
+
+  useEffect(() => {
+    console.error(error.message);
+  }, [error]);
+
+  return (
+    <div>
+      <h2>검색 과정에서 오류가 발생했습니다.</h2>
+      {/* <button onClick={() => reset()}>다시 시도 </button> */}
+      {/* <button onClick={() => window.location.reload()}>다시 시도 </button> */}
+      <button onClick={() => {
+        startTransition(() => {
+            router.refresh()
+            reset()
+        });
+      }}>다시 시도 </button>
+    </div>
+  );
+}
